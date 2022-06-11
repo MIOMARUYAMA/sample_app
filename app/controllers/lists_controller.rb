1f393341fc7d034ee775242cfcb2f323378d5679
MIOMARUYAMA <106859222+MIOMARUYAMA@users.noreply.github.com>
2022-06-11 07:48:48 +0000

    [finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,15 @@
 class ListsController < ApplicationController
   def new
+    @list=List.new
+  end
+  # 投稿を保存するためのcreatアクションの作成
+  def creat
+    # フォームに記述された文字やデータを受け取る（データを受け取り新規登録するためのインスタンス作成）
+    list=List.new(list_params)
+    # データをDBに保存するためのsaveメソッド実行
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to'/top'
   end
 
   def index
@@ -9,5 +19,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
